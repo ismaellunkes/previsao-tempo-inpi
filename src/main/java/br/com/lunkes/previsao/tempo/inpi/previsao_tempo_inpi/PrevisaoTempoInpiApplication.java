7aa17d6c9027d1d0c44a62e2bb6b7d4ecef16e12
ismaellunkes <lunkes86@gmail.com>
2024-07-06 21:36:50 -0300

    Inicializando o projeto

---
+++ src/main/java/br/com/lunkes/previsao/tempo/inpi/previsao_tempo_inpi/PrevisaoTempoInpiApplication.java
@@ -0,0 +1,13 @@
+package br.com.lunkes.previsao.tempo.inpi.previsao_tempo_inpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrevisaoTempoInpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrevisaoTempoInpiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lunkes/previsao/tempo/inpi/previsao_tempo_inpi/PrevisaoTempoInpiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lunkes.previsao.tempo.inpi.previsao_tempo_inpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrevisaoTempoInpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
