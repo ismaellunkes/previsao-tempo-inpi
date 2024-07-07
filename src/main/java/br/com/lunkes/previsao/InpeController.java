492cb8cb34ffb3f804ee4917f9e633c3afe4a5c9
ismaellunkes <lunkes86@gmail.com>
2024-07-07 19:46:16 -0300

    Variaveis de ambiente: ajuste para ler variaveis de ambiente setadas em arquivo .env na raiz do projeto.

--- src/main/java/br/com/lunkes/previsao/InpeController.java
+++ src/main/java/br/com/lunkes/previsao/InpeController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping("/inpe")
 public class InpeController {
@@ -16,6 +15,7 @@ public class InpeController {
     public InpeController(InpeService inpeService) {
         this.inpeService = inpeService;
     }
+
     @GetMapping("/docs")
     public String getInpeData() {
         return inpeService.getInpeData();
--- src/main/java/br/com/lunkes/previsao/PrevisaoTempoInpiApplication.java
+++ src/main/java/br/com/lunkes/previsao/PrevisaoTempoInpiApplication.java
@@ -3,10 +3,14 @@ package br.com.lunkes.previsao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class PrevisaoTempoInpiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(PrevisaoTempoInpiApplication.class, args);
 	}
 
